e1938bbe4eb753025448c5fbfedf617b1e6dd07e
razaqadri1987-cpu <razaqadri.1987@gmail.com>
2026-05-12 00:41:29 +0530

    Update App.jsx
    Upload website files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function App() {
+  return (
+    <div style={{padding:"40px", fontFamily:"Arial"}}>
+      <h1>Homeline Interior Website</h1>
+      <p>Website Successfully Running 🚀</p>
+    </div>
+  );
+}
